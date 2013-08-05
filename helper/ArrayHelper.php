abc2ded25d890475da30e4089cf6521e31ed92d7
Timothy Chandler <tim@praxisbt.com>
2013-08-05 19:15:15 +0800

    Added more array helper functions.

--- helper/ArrayHelper.php
+++ helper/ArrayHelper.php
@@ -85,5 +85,102 @@ namespace nutshell\helper
 			}
 			return $result;
 		}
+		
+		static public function flatten(Array $array)
+		{
+			$newArray=array();
+			foreach ($array as $val)
+			{
+				if (is_array($val))
+				{
+					$newArray=array_merge($newArray,self::flatten($val));
+				}
+				else
+				{
+					$newArray[]=$val;
+				}
+			}
+			return $newArray;
+		}
+		
+		static public function without(Array &$array,$without)
+		{
+			if (!is_array($without))
+			{
+				$without=array($without);
+			}
+			for ($i=0,$j=count($without); $i<$j; $i++)
+			{
+				foreach ($array as $key=>$val)
+				{
+					if ($without[$i]==$val)
+					{
+						unset($array[$key]);
+					}
+				}
+				ksort($array);
+			}
+		}
+		
+		static public function withoutKey(Array $array,$without)
+		{
+			if (!is_array($without))
+			{
+				$without=array($without);
+			}
+			$newArray=array();
+			foreach ($array as $key=>$val)
+			{
+				if (!in_array($key,$without))
+				{
+					$newArray[$key]=$val;
+				}
+			}
+			return $newArray;
+		}
+		
+		static public function closest(Array $array,$value)
+		{
+			sort($array);
+			$closest=$array[0];
+			for ($i=1,$j=count($array),$k=0; $i<$j; $i++,$k++)
+			{
+				$middleValue=($array[$i]-$array[$k])/2+$array[$k];
+				if ($value>=$middleValue)
+				{
+					$closest=$array[$i];
+				}
+			}
+			return $closest;
+		}
+		
+		static public function trim(Array $array)
+		{
+			foreach($array as $key=>$val)
+			{
+				if (is_string($val))
+				{
+					$array[$key]=trim($val);
+				}
+			}
+			return $array;
+		}
+		
+		static public function trimKeys(Array $array)
+		{
+			$newArray=array();
+			foreach($array as $key=>$val)
+			{
+				if (is_string($key))
+				{
+					$newArray[trim($key)]=$val;
+				}
+				else
+				{
+					$newArray[$key]=$val;
+				}
+			}
+			return $newArray;
+		}
 	}
 }
