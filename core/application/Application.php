a7edae657b17791b063ab73d3e4d2485a135c4ed
Timothy Chandler <tim@praxisbt.com>
2013-08-15 03:26:16 +0800

    Fix for application reference consistancy.

--- core/application/Application.php
+++ core/application/Application.php
@@ -30,12 +30,11 @@ namespace nutshell\core\application
 		{
 			parent::__construct();
 			$applicationName	=ObjectHelper::getBaseClassName(get_called_class());
-			$applicationRef		=strtolower($applicationName);
+			$applicationRef		=lcfirst($applicationName);
 			$this->nutshell		=Nutshell::getInstance();
 			$this->config		=$this->nutshell->setupApplicationConfig($applicationName);
 			$this->application	=Nutshell::getInstance()->application;
 			$this->request		=Nutshell::getInstance()->request;
-			
 			$this->nutshell->getPluginLoader()->registerContainer($applicationRef.'_plugin',APP_HOME.$applicationRef._DS_.'plugin'._DS_,'application\\'.$applicationRef.'\plugin\\');
 			
 			if (method_exists($this,'init'))
